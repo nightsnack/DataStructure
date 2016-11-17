86a05807a87b7250dd3108179e6965e07fa8597b
NightSnack <chiyexiao@icloud.com>
2016-11-18 00:10:53 +0800

    图类完成

--- src/com/nc/Graph/LinkedGraph.java
+++ src/com/nc/Graph/LinkedGraph.java
@@ -1,5 +1,7 @@
 package com.nc.Graph;
 
+import java.util.ArrayList;
+
 public class LinkedGraph implements Graph {
 
 	private int n; //vertex
@@ -10,7 +12,7 @@ public class LinkedGraph implements Graph {
 	
 	public LinkedGraph (int n, int t)
 	{
-		if (n<1 || t>0 || t>3) {
+		if (n<1 || t<0 || t>3) {
 			System.out.println("Initialize error,wrong varibles");
 			System.exit(1);
 		}
@@ -67,79 +69,265 @@ public class LinkedGraph implements Graph {
 
 	@Override
 	public int graphType() {
-		// TODO Auto-generated method stub
-		return 0;
+		// 类型
+		return type;
 	}
 
 	@Override
 	public int vertices() {
-		// TODO Auto-generated method stub
-		return 0;
+		// 顶点数
+		return n;
 	}
 
 	@Override
 	public int edges() {
-		// TODO Auto-generated method stub
-		return 0;
+		// 边数
+		return e;
 	}
 
+	
 	@Override
 	public boolean find(int i, int j) {
-		// TODO Auto-generated method stub
+		if(i<0||i>n-1||j<0||j>n-1||i==j) {
+			System.out.println("编号无效，退出");
+			System.exit(0);
+		}
+		EdgeNode p = a[i];
+		while (p!=null) {
+			if(p.adjvex == j) return true;
+			p = p.next;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean putEdge(EdgeElement theEdge) {
-		// TODO Auto-generated method stub
-		return false;
+		int v1,v2;
+		v1 = theEdge.fromvex;v2 = theEdge.endvex;
+		if(v1<0||v1>n-1||v2<0||v2>n-1||v1==v2) {
+			System.out.println("边顶点序号无效，退出");
+			System.exit(1);
+		}
+		EdgeNode p = a[v1];
+		while (p!=null) {
+			if (p.adjvex == v2)
+				break;
+			p = p.next;
+		}
+		if (p == null)
+			e++;
+		else {
+			System.out.println("alerady Exist");
+		}
+		return true;
 	}
 
 	@Override
 	public boolean removeEdge(int i, int j) {
 		// TODO Auto-generated method stub
-		return false;
+		if (i<0 || i>n-1 || j<0 || j>n-1 || i==j) {
+			System.out.println("顶点序号无效");
+			System.exit(1);
+		}
+		EdgeNode p = a[i], q = null;
+		while (p != null) {
+			if (p.adjvex == j) break;
+			q = p;p = p.next;
+		}
+		
+		if (p == null) {
+			System.out.println("Edge prepared to delete do not exist!");
+			return false;
+		}
+		
+		if(q == null)
+			p = p.next;
+		else 
+			q.next = p.next;
+		
+		if (type==0 || type == 1) {
+			EdgeNode p1 = a[j],q1 =null;
+			while (p1!=null) {
+				if (p1.adjvex == i)  break;
+				q1 = p1;p1 = p1.next;
+			}
+			if (q1 == null)
+				p1 = p1.next;
+			else
+				q1.next = p1.next;
+		}
+		e--;
+		return true;
 	}
 
 	@Override
 	public int degree(int i) {
 		// TODO Auto-generated method stub
-		return 0;
+		if (i<0 || i>n-1) {
+			System.out.println("vertex varible is invalid!");
+			System.exit(1);
+		}
+		int k = 0;
+		if (type == 0 ||type ==1 ) {
+			EdgeNode p = a[i];
+			while (p!= null) {
+				p = p.next;
+				k++;
+			}
+			return k;
+		}
+		else
+			return inDegree(i)+outDegree(i);
 	}
 
 	@Override
 	public int inDegree(int i) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (i<0 || i>n-1) {
+			System.out.println("vertex varible is invalid!");
+			System.exit(1);
+		}
+		
+		if (type == 0 ||type ==1 ) {
+			return degree(i);
+		}
+		int k = 0;
+		for (int j = 0; j < n; j++) {
+			EdgeNode p = a[j];
+			while (p!=null) {
+				if(p.adjvex == i)
+					k++;
+				p = p.next;
+			}
+		}
+		return k;
 	}
 
 	@Override
 	public int outDegree(int i) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (i<0 || i>n-1) {
+			System.out.println("vertex varible is invalid!");
+			System.exit(1);
+		}
+		int k=0;
+		EdgeNode p = a[i];
+		while (p != null) {
+			k++;
+			p = p.next;
+		}
+		return k;
 	}
 
 	@Override
 	public void output() {
 		// TODO Auto-generated method stub
-
+		int i,j;
+		System.out.print("V={");
+		for (i = 0; i<n-1; i++ ) System.out.print(i+ ",");
+		System.out.println(n-1+"}");
+		System.out.print("E={");
+		for (i=0; i<n; i++) {
+			EdgeNode p = a[i];
+			while (p!=null) {
+				j = p.adjvex;
+				switch (type) {
+				case 0:
+					if(i<j) System.out.print("("+i+","+j+"),");
+					break;
+				case 1:
+					System.out.print("<"+i+","+j+">,");
+					break;
+				case 2:
+					if (i<j) System.out.print("("+i+","+j+")"+p.weight+",");
+					break;
+				case 3:
+					System.out.print("<"+i+","+j+">"+p.weight+",");
+					break;
+				}
+				p = p.next;	
+			}
+		}
+		System.out.println("}");
 	}
 
+	/**
+	 * DRIVE
+	 * @param v 开始节点号
+	 */
 	@Override
 	public void depthFirstSearch(int v) {
-		// TODO Auto-generated method stub
-
+		boolean[] visited = new boolean[n];
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = false;
+		}
+		dfs(v,visited);
+		System.out.println();
+		
 	}
-
+	
+	/**
+	 * WORK
+	 * @param i	节点
+	 * @param visited	标记数组
+	 */
+	private void dfs(int i,boolean[] visited) {
+		System.out.print(i+" ");
+		visited[i] = true;
+		EdgeNode p = a[i];
+		while(p!=null) {
+			int j = p.adjvex;
+			if (!visited[j])
+				dfs(j, visited);
+			p = p.next;
+		}
+	}
+	
+	/**
+	 * DRIVE
+	 * @param v 开始节点号
+	 */
 	@Override
 	public void breadthFirstSearch(int v) {
-		// TODO Auto-generated method stub
-
+		boolean[] visited = new boolean[n];
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = false;
+		}
+		bfs(v,visited);
+		System.out.println();
+		
 	}
+	
+	/**
+	 * WORK
+	 * @param i	节点
+	 * @param visited	标记数组
+	 */
+	private void bfs(int i,boolean[] visited) {
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		System.out.print(i+" ");
+		visited[i] = true;
+		arrayList.add(i);
+		while(!arrayList.isEmpty()) {
+			int k = arrayList.get(0);
+			arrayList.remove(0);
+			EdgeNode p = a[k];
+			while (p!=null) {
+				int j = p.adjvex;
+				if (!visited[j]) {
+					System.out.print(j+" ");
+					visited[j]=true;
+					arrayList.add(j);
+				}
+				p = p.next;
+			}
+				
+		}
+	}
+
 
 	@Override
 	public void clearGraph() {
 		// TODO Auto-generated method stub
+		n = e = type=0;a=null;
 
 	}
 
