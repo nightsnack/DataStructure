b1c59684d9d6e8d06613944af04e09feaa24790f
NightSnack <chiyexiao@icloud.com>
2016-11-21 17:21:04 +0800

    Finish LinearList

--- src/com/nc/LinearList/List.java
+++ src/com/nc/LinearList/List.java
@@ -1,10 +1,38 @@
 package com.nc.LinearList;
 
 public interface List {
+	/**
+	 * 返回线性表中的第pos个元素的值	return the value of position 'pos'
+	 * @param pos
+	 * @return
+	 */
 	Object value(int pos);	//返回线性表中的第pos个元素的值	
+	
+	/**
+	 * 向第pos位置插入一个元素obj add the element 'obj' to the position 'pos'
+	 * @param obj
+	 * @param pos
+	 * @return T/F
+	 */
 	boolean	add(Object obj, int pos);	//向第pos位置插入一个元素obj
+	/**
+	 * remove the element in the position pos
+	 * @param pos
+	 * @return the object deleted just now
+	 */
 	Object remove(int pos);		//删除第pos个元素并返回，若不存在返回空
+	/**
+	 * find the element which value equals to value 'obj' from the front, return the position
+	 * @param obj
+	 * @return position
+	 */
 	int find(Object obj);		//从头查找等于obj值的第一个元素，返回序号
+	/**
+	 * replace element pos with obj
+	 * @param obj
+	 * @param pos
+	 * @return
+	 */
 	boolean modify(Object obj,int pos);		//用obj修改线性表中的给定序号pos的元素值
 	boolean isEmpty();		//判断线性表是否为空
 	int size();		//返回线性表的长度
--- src/com/nc/LinearList/SequenceList.java
+++ src/com/nc/LinearList/SequenceList.java
@@ -1,71 +1,166 @@
 package com.nc.LinearList;
 
 public class SequenceList implements List {
+	final int minSize = 10;
+	private Object[] listArray;
+	private int len;
+	public SequenceList() {
+		len = 0;
+		listArray = new Object[minSize];
+	}
+	
+	public SequenceList(int n) {
+		if (n<minSize) {
+			n = minSize;
+		}
+		len = 0;
+		listArray = new Object[n];
+	}
 
 	@Override
 	public Object value(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos<1 || pos>=len) {
+			System.out.println("pos is invalid");
+			return false;
+		}
+		return listArray[pos-1];
 	}
 
 	@Override
 	public boolean add(Object obj, int pos) {
-		// TODO Auto-generated method stub
-		return false;
+		if (pos<1 || pos>len+1) {
+			System.out.println("pos is invalid");
+			return false;
+		}
+		if (len == listArray.length) {
+			Object[] objArr = new Object[2*len];
+			for(int i = 0; i<listArray.length; i++) {
+				objArr[i] = listArray[i];
+			}
+			listArray = objArr;
+		}
+		for(int i = len-1; i>=pos-1; i--) {
+			listArray[i+1] = listArray[i];
+		}
+		listArray[pos-1] = obj;
+		len++;
+		return true;
 	}
 
 	@Override
 	public Object remove(int pos) {
-		// TODO Auto-generated method stub
-		return null;
+		if (pos<1 || pos>len) {
+			System.out.println("pos is invalid");
+			return null;
+		}
+		Object x = listArray[pos-1];
+		for (int i = pos-1; i<len-1; i++ ) {
+			listArray[i] = listArray[i+1];
+		}
+		len --;
+		return x;
 	}
 
 	@Override
 	public int find(Object obj) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i;
+		for (i = 0; i < listArray.length; i++) {
+			if(listArray[i].equals(obj))
+				return ++i;
+		}
+		return -1;
 	}
 
 	@Override
 	public boolean modify(Object obj, int pos) {
-		// TODO Auto-generated method stub
-		return false;
+		if (pos<1 || pos>len) {
+			System.out.println("pos is invalid");
+			return false;
+		}
+		listArray[pos-1] = obj;
+		return true;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return len==0;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return len;
 	}
 
 	@Override
 	public void forward() {
 		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < len; i++) {
+			System.out.println(listArray[i].toString());
+		}
 	}
 
 	@Override
 	public void backward() {
-		// TODO Auto-generated method stub
-		
+		for (int i = len-1; i >= 0; i--) {
+			System.out.println(listArray[i].toString());
+		}
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		len=0;
 		
 	}
 
 	@Override
 	public List sort() {
 		// TODO Auto-generated method stub
-		return null;
+		SequenceList list = new SequenceList(len);
+		list.len = len;
+		for (int i = 0; i < len; i++) {
+			list.listArray[i] = listArray[i];
+		}
+		for (int i = 0; i<list.len; i++) {
+			Object x = list.listArray[i];
+			int j;
+			for (j = i-1; j>=0; j-- ) {
+				Object y = list.listArray[j];
+				if(Integer.valueOf(x.toString()) < Integer.valueOf(y.toString())) {
+					list.listArray[j+1] = list.listArray[j];
+				} else
+				{
+					break;
+				}
+			}
+			list.listArray[j+1] = x;
+		}
+		return list;
 	}
+	
+	public static void main (String [] args) {
+		List list = new SequenceList(10);
+		int[] a = {20,16,38,42,29};
+		for(int i = 0; i<a.length; i++) {
+			list.add(a[i], i+1);
+		}
+		int n1 = (Integer)list.remove(2);
+		list.add(80, 3);
+//		list.add(80, 20);
+		int n2 = (Integer)list.value(4);
+		list.modify(33, 4);
+		System.out.println("n1="+n1+" n2="+n2);
+		System.out.println("list length:"+list.size());
+		list.forward();
+		
+		List list1 = list.sort();
+		System.out.println("list1 length:"+list1.size());
+		list1.forward();
+		System.out.println();
+		list1.backward();
+		System.out.println("list1 length:"+list1.size());
+	}
+
 
 }
